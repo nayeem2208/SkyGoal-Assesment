bba6819796b60cce43f43add2748d2f813b0e9d5
Nayeem <nayeem2281998@gmail.com>
2023-12-06 11:15:44 +0530

    final commit after setting up of all frontend

--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -1,13 +1,33 @@
 import express from 'express'
+import path from "path";
+import multer from "multer";
 import { editDetails, getUserDetails, login, register } from '../controllers/userController.js'
 import authcheck from '../middleware/auth.js'
+
 const router=express.Router()
+
+const storage = multer.diskStorage({
+    destination: (req, file, cb) => {
+      cb(null, "backend/public/images");
+    },
+    filename: (req, file, cb) => {
+      cb(
+        null,
+        file.fieldname + "_" + Date.now() + path.extname(file.originalname)
+      );
+    },
+  });
+  
+  const upload = multer({
+    storage: storage,
+  });
+  
 
 // router.get('/check',login)
 router.post('/login',login)
 router.post('/signup',register)
 router.get('/getUserData',authcheck,getUserDetails)
-router.patch('/editDetails',authcheck,editDetails)
+router.patch('/editDetails',authcheck,upload.single("image"),editDetails)
 
 
 export default router
--- frontend/src/components/editDetails.jsx
+++ frontend/src/components/editDetails.jsx
@@ -1,96 +1,141 @@
 import React, { useState, useEffect } from "react";
 import { useAuth } from "../utils/userContext";
 import axiosInstance from "../utils/axiosInstance";
+import {motion} from 'framer-motion'
 
 function EditDetails({ setEditDetails }) {
-  const { user, setUser } = useAuth();
-  const [userData, setUserData] = useState({});
-
-
-  useEffect(() => {
-    setUserData({
-      name: user ? user.name : "",
-      phone: user ? user.phone : "",
-      email: user ? user.email : "",
-      address: user ? user.Address : "",
+    const { user, setUser } = useAuth();
+    const [userData, setUserData] = useState({
+      name: "",
+      phone: "",
+      email: "",
+      address: "",
+      profilePic: null, // Added for profile picture
     });
-  }, [user]);
-
-
-  const handleChange = (e) => {
-    const { name, value } = e.target;
-    setUserData((prevUserData) => ({
-      ...prevUserData,
-      [name]: value,
-    }));
-  };
-
   
-  const handleSubmit = async (e) => {
-    e.preventDefault();
-    try {
-      const res = await axiosInstance.patch("/editDetails", { userData });
-      setUser(res.data.saveUser);
-      setEditDetails((prevValue) => !prevValue);
-    } catch (error) {
-      console.log(error);
-    }
-  };
+    useEffect(() => {
+      if (user) {
+        setUserData({
+          name: user.name || "",
+          phone: user.phone || "",
+          email: user.email || "",
+          address: user.Address || "",
+          profilePic: user.profilePic || null,
+        });
+      }
+    }, [user]);
+  
+    const handleChange = (e) => {
+      const { name, value, files } = e.target;
+  
+      // If the input is a file input, set the profilePic to the selected file
+      if (name === "image" && files && files.length > 0) {
+        setUserData((prevUserData) => ({
+          ...prevUserData,
+          profilePic: files[0],
+        }));
+      } else {
+        setUserData((prevUserData) => ({
+          ...prevUserData,
+          [name]: value,
+        }));
+      }
+    };
+  
+    const handleSubmit = async (e) => {
+      e.preventDefault();
+      try {
+        const formData = new FormData();
+        formData.append("name", userData.name);
+        formData.append("phone", userData.phone);
+        formData.append("email", userData.email);
+        formData.append("address", userData.address);
+        formData.append("image", userData.profilePic); // Append profilePic as a file
+  
+        const res = await axiosInstance.patch("/editDetails", formData, {
+          headers: {
+            "Content-Type": "multipart/form-data",
+          },
+        });
+  
+        setUser(res.data.saveUser);
+        setEditDetails((prevValue) => !prevValue);
+      } catch (error) {
+        console.log(error);
+      }
+    };
 
   return (
     <div className="flex justify-center bg-white w-3/5 rounded-2xl shadow-lg p-8">
       <div className="flex flex-col w-full">
-        <p className="text-center font-bold text-gray-800 text-3xl mb-4">
+        <p className="text-center font-bold text-sky-700 text-3xl mb-4">
           Edit Details
         </p>
-        <div className="text-xl">
-          <form onSubmit={handleSubmit}>
-            <div className="mb-4 shadow-lg">
-              <input
+        <div className="text-lg text-sky-700">
+          <form onSubmit={handleSubmit} >
+            <motion.div className="mb-4 shadow-lg " whileHover={{scale:1.01}}>
+              <motion.input
                 type="text"
                 name="name"
+                whileHover={{backgroundColor:'#33ace8',color:'white'}}
                 value={userData.name}
                 className="w-full border rounded py-2 px-3 focus:outline-none focus:shadow-outline"
                 placeholder="Name"
                 onChange={handleChange}
               />
-            </div>
-            <div className="mb-4 shadow-lg">
-              <input
+            </motion.div>
+            <motion.div className="mb-4 shadow-lg" whileHover={{scale:1.01}}>
+              <motion.input
                 type="text"
                 name="phone"
+                whileHover={{backgroundColor:'#33ace8',color:'white'}}
                 value={userData.phone}
                 className="w-full border rounded py-2 px-3 focus:outline-none focus:shadow-outline"
                 placeholder="Phone"
                 onChange={handleChange}
               />
-            </div>
-            <div className="mb-4 shadow-lg">
-              <input
+            </motion.div>
+            <motion.div className="mb-4 shadow-lg" whileHover={{scale:1.01}}>
+              <motion.input
                 type="text"
                 name="email"
+                whileHover={{backgroundColor:'#33ace8',color:'white'}}
                 value={userData.email}
                 className="w-full border rounded py-2 px-3 focus:outline-none focus:shadow-outline"
                 placeholder="Email"
                 onChange={handleChange}
               />
-            </div>
-            <div className="mb-4 shadow-lg">
-              <textarea
+            </motion.div>
+            <motion.div className="mb-4 shadow-lg" whileHover={{scale:1.01}}>
+              <motion.textarea
                 type="text"
                 name="address"
+                whileHover={{backgroundColor:'#33ace8',color:'white'}}
                 value={userData.address}
                 className="w-full border rounded py-2 px-3 focus:shadow-outline"
                 placeholder="Address"
                 onChange={handleChange}
               />
+            </motion.div>
+            <div>
+                <input type="file" name="image" onChange={handleChange}/>
             </div>
-            <button
+            <div className="flex  my-4">
+            <motion.button
               type="submit"
-              className="mt-4 bg-cyan-600 text-white py-2 px-4 rounded hover:bg-cyan-700 focus:outline-none focus:shadow-outline"
+              
+              className="mt-4 mx-3 bg-cyan-600 text-white py-2 px-4 rounded hover:bg-cyan-700 focus:outline-none focus:shadow-outline"
             >
               Save Changes
-            </button>
+            </motion.button>
+            <motion.button
+            
+              onClick={()=>setEditDetails(false)}
+              className="mt-4 mx-3 bg-cyan-600 text-white py-2 px-4 rounded hover:bg-cyan-700 focus:outline-none focus:shadow-outline"
+            >
+              cancel
+            </motion.button>
+            </div>
           </form>
         </div>
       </div>
